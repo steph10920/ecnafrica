ba7cd43cad9033ecb20dc79ed583eb15f4cb268e
Steph10920 <sashioyas@gmail.com>
2025-10-23 18:31:42 +0300

    programs context succesfully added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/About";
+import Programs from "./pages/Programs"; // ✅ Added import
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/programs" element={<Programs />} /> {/* ✅ Programs route */}
       </Routes>
     </Router>
   );
