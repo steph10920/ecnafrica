41b649e56e246f55051901f92981107850df059a
Steph10920 <sashioyas@gmail.com>
2025-11-24 11:04:00 +0300

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,14 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
         <p>© {new Date().getFullYear()} ECN Africa. All rights reserved.</p>
         <div className="flex space-x-4 mt-3 md:mt-0">
-          <a href="https://share.google/AAnf7YeukphQ5GaHa" className="hover:text-green-300">Facebook</a>
+          <a
+            href="https://share.google/AAnf7YeukphQ5GaHa"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-green-300"
+          >
+            Facebook
+          </a>
         </div>
       </div>
     </footer>
